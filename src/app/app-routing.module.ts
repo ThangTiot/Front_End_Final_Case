2fd5d33009a5d7c0eb7bd85e2fee6c7863e8a601
ThangTiot <nguyendatthanguet@gmail.com>
2022-10-06 09:35:16 +0700

    chuyen sang tab about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {UsersComponent} from "./component/users/users.component";
 import {NewsFeedComponent} from "./component/news-feed/news-feed.component";
 import {TimeLineComponent} from "./component/time-line/time-line.component";
+import {AboutComponent} from "./component/about/about.component";
 
 const routes: Routes = [
   { path: '', component: UsersComponent },
   { path: 'newsFeed', component: NewsFeedComponent },
   { path: 'timeLine/:id', component: TimeLineComponent },
+  { path: 'about/:id', component: AboutComponent },
 ];
 
 @NgModule({
